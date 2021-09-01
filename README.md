54006d5d8b93bf48d993f470bb8f320554810a75
HyrestXDD <89876363+HyrestxD@users.noreply.github.com>
2021-09-01 09:07:48 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Context ve Slash Komutları
+- Altyapının full halini paylaşmadım içindeki herşey tamdır, sadece ekstra komutları eklemedim
+- İçerisinde ban mute ve kick komutları bulunmakta 
+- Altyapının boş hali bana ait değildir ^^
+
+
+## Yardım & Destek İçin
+- Discord:[Hyrest ✭#0001](https://discord.com/channels/@me/862997440256278539)
+- Instagram: [ff_orhan0](https://www.instagram.com/ff_orhan0/?hl=tr)
+
+## Önemli
+- Botunuzu sunucuya eklerken application.commands scope'u mutlaka işaretli olmalıdır.
+
+https://media.discordapp.net/attachments/871492337480630292/882505819235684402/unknown.png
---
+++ config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    token: "BOTUNUZUN TOKENi",
+    prefx: "BOTUNUZUN PREFİXİ",
+    owner: "SİZİN IDNİZ",
+    mongoURL: "SİZİN IDNİZ",
+    sunucuID: "SUNUCU ID",
+    
+    roles: {
+        muted: "MUTELİ ROL ID",
+    }, 
+
+    embed: {
+        sunucuAdı: "SUNUCU İSMİ",
+        embedRenk: "EMBED RENK (RANDOM) YAZMANIZ ÖNERİLİR",
+        authorTag: "BOTUNUZUN BİOSU",
+    },
+
+    staffs: {
+        banStaff: "BAN YETKİLİSİNİN IDSİ",
+        muteStaff: "MUTE YETKİLİSİ ID",
+    },
+
+    logs:{
+        mutelog: "MUTE LOG",
+        jaillog: "JAİL LOG",
+    },
+    
+    emojis: {
+        OnayE: "ONAY EMOJİSİ ID",
+        IptalE: "İPTAL EMOJİSİ ID",
+    }
+}
