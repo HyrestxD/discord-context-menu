efa45d0573d15f5f073b263a43e1472c58e8b035
HyrestXDD <89876363+HyrestxD@users.noreply.github.com>
2021-09-01 13:50:56 +0300

    Add files via upload

---
+++ commands/info/ping.js
@@ -0,0 +1,16 @@
+const { Message, Client } = require("discord.js");
+
+module.exports = {
+    name: "ping",
+    aliases: ['p'],
+    /**
+     *
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
+     */
+    run: async (client, message, args) => {
+        message.channel.send(`${client.ws.ping} ws ping`);
+    },
+};
+ // Normal Komutlar
